0b0dd4f30e2152ad3f3691dd2a7b8022d0876438
John Ross <johnyboy11@hotmail.co.uk>
2021-06-20 09:51:00 +0100

    "Completed instrument super class"

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -1,4 +1,29 @@
 package instruments;
 
-public class Instrument {
+public abstract class Instrument {
+
+    private String brand;
+    private String materials;
+    private String colour;
+
+
+
+    public Instrument(String brand, String materials, String colour) {
+        this.brand = brand;
+        this.materials = materials;
+        this.colour = colour;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getMaterials() {
+        return materials;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
 }
